ddb9f40882c4ad9d346cd40ee4a9d5039004624e
antlev <antoinelevy@hotmail.com>
2017-07-21 20:01:08 +0200

    add function to draw a cercle of cities, with a number of cities passed as a parameter

--- Tsp.py
+++ Tsp.py
@@ -1,9 +1,55 @@
+from tkinter import *
+from math import *
 
-class Tsp:
+# Importation des librairies nécéssaire au bon fonctionnement du programme.
+# Tkinter pour l'interface graphique
+# urllib pour les schémas internet
+# os pour dialoguer avec le systeme
+import tkinter as tk
+import urllib as url
+import os
+
+
+class Tsp(Frame):
+    '''Application principale'''
 
     def __init__(self, nb_city):
+        '''constructeur'''
         self.nb_city = nb_city
-        self.draw_circle()
+        self.fen = Tk()
+        self.fen.title('Tkinter')
 
-    def draw_circle(self):
+        self.bou_action = Button(self.fen)
+        self.bou_action.config(text='Draw', command=self.draw)
+        self.bou_action.pack()
 
+        self.bou_quitter = Button(self.fen)
+        self.bou_quitter.config(text='Quitter', command=self.fen.destroy)
+        self.bou_quitter.pack()
+
+    def run(self):
+        self.fen.mainloop()
+        self.draw()
+
+    def draw(self):
+        width = 900
+        height = 900
+        pi = 3.14
+        canvas = Canvas(self.fen, width=width, height=height, background='white')
+
+        teta = 0
+        r = 250
+        cities = []
+        while teta < 360:
+            print("teta "+repr(teta)+ " sin(teta) >" +repr(sin(teta)) +" cos(teta) >"+ repr(cos(teta)))
+            cities.append((r*sin(teta*pi/180)+500,r*cos(teta*pi/180)+500))
+            teta += 360/self.nb_city
+
+
+        canvas.create_polygon(cities, fill="magenta", outline="yellow", width=3)
+        canvas.pack()
+
+
+
+
+        # canvas.pack()
--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from Genetic import Genetic
+from Tsp import Tsp
+from tkinter import *
+
 
 def main():
     """ Main function. """
-    target = "Hello, World !"
-    pop_size = 10
-    best_pourcentage = 0.30
-    crossover_rate = 0.70
-    mutation_rate = 0.5
-    gen = Genetic(target, pop_size, best_pourcentage, crossover_rate, mutation_rate)
+    # target = "Hello, World !"
+    # pop_size = 10
+    # best_pourcentage = 0.30
+    # crossover_rate = 0.70
+    # mutation_rate = 0.5
+    # gen = Genetic(target, pop_size, best_pourcentage, crossover_rate, mutation_rate)
+    #
+    # iterations = 1000
+    # best_fitness_exp =0
+    # gen.start(iterations, best_fitness_exp)
 
-    iterations = 1000
-    best_fitness_exp =0
-    gen.start(iterations, best_fitness_exp)
+
+
+    app = Tsp(60)
+    app.run()
 
 if __name__ == '__main__':
     main()
